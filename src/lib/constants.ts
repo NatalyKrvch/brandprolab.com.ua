bbad43f644bacd927a3987f46b8657d9aa63351d
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-01-20 20:08:32 +0200

    feat: add constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const ALL_DATA_QUERY = `
+{
+  "header": *[_type == "header"][0],
+  "hero": *[_type == "hero"][0],
+  "cases": *[_type == "cases"][0],
+  "services": *[_type == "services"][0],
+  "testimonials": *[_type == "testimonialsSection"][0],
+  "subscribe": *[_type == "subscribeSection"][0],
+  "video": *[_type == "videoSection"][0],
+  "expertise": *[_type == "expertiseSection"][0],
+  "diagnostic": *[_type == "diagnosticSection"][0]
+}
+`;
