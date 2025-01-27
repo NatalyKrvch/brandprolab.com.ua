153f76176c693feb0e3c4d0f2f40fcc389f3ab8a
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-01-27 20:10:20 +0200

    feat: add global constants for icon sizes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,6 +14,44 @@ export const ALL_DATA_QUERY = `
 
 export const REVALIDATE = 60;
 
+export const iconOffsetCoefficient = 3;
+
+export const backgroundCircleSizes = {
+  s: {
+    mobile: 36,
+    tablet: 42,
+    desktop: 48,
+  },
+  m: {
+    mobile: 48,
+    tablet: 56,
+    desktop: 64,
+  },
+  l: {
+    mobile: 68,
+    tablet: 80,
+    desktop: 108,
+  },
+};
+
+export const iconSizes = {
+  s: {
+    mobile: 36,
+    tablet: 42,
+    desktop: 48,
+  },
+  m: {
+    mobile: 52,
+    tablet: 60,
+    desktop: 64,
+  },
+  l: {
+    mobile: 64,
+    tablet: 72,
+    desktop: 96,
+  },
+};
+
 // Test IDs
 export const INTERNAL_LINK_TEST_ID = 'internal-link-test-id';
 export const EXTERNAL_LINK_TEST_ID = 'external-link-test-id';
