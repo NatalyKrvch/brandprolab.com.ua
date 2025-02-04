2520bdbfe9ba41c871c99793b9695679cec14330
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-04 13:23:28 +0200

    feat: update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,31 +15,48 @@ export default {
         symbols: ['Mariupol-Symbols', 'sans-serif'],
       },
       fontSize: {
-        sm: '16px',
-        base: '18px',
-        lg: '22px',
-        xl: '32px',
-        '2xl': '52px',
+        '16': '16px',
+        '18': '18px',
+        '20': '20px',
+        '22': '22px',
+        '24': '24px',
+        '32': '32px',
+        '52': '52px',
+      },
+      lineHeight: {
+        '26': '26px',
+      },
+      letterSpacing: {
+        '0.4': '0.4px',
+        '0.44': '0.44px',
+        '0.48': '0.48px',
       },
       colors: {
         white: '#FFFFFF',
+        whiteOpacity: 'rgba(255, 255, 255, 0.3)',
+
         black: '#253737',
+
         teal: {
           dark: '#06BCAD',
           darkOpacity: 'rgba(6, 188, 173, 0.1)',
+          darkTranslucent: 'rgba(6, 188, 173, 0.3)',
           medium: '#0BDAC9',
           light: '#E7FAF9',
           lightOpacity: 'rgba(231, 250, 249, 0.8)',
         },
+
         gray: {
           dark: '#425A62',
           medium: '#B2DCDC',
           light: '#F5F9F9',
         },
+
         blue: {
           default: '#156FBA',
           dark: '#32607D',
         },
+
         red: {
           default: '#D44C62',
         },
