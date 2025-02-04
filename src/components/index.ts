cb4eeb1dcd1b2bc8558cfbb63785268d714b1349
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-04 13:25:41 +0200

    feat: add reexports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { Container } from './Container';
 export { ExternalLink } from './ExternalLink';
 export { InternalLink } from './InternalLink';
 export { Icon } from './Icon';
+export { Label } from './Label';
+export { Button } from './Button';
