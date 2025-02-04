1b25150244e78af024dd73fd059cd0c4a2b3ffeb
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-04 13:36:42 +0200

    test: add tests & snapshots for UnderlinedText component

---
+++ src/components/UnderlinedText/UnderlinedText.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import UnderlinedText from './UnderlinedText';
+import '@testing-library/jest-dom';
+import { UNDERLINED_TEXT_TEST_ID } from '@/lib/constants';
+
+describe('UnderlinedText component', () => {
+  const defaultProps = {
+    children: 'Underlined Text',
+  };
+
+  it('renders the UnderlinedText component correctly', () => {
+    const { asFragment } = render(<UnderlinedText {...defaultProps} />);
+    const textElement = screen.getByTestId(UNDERLINED_TEXT_TEST_ID);
+
+    expect(textElement).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('displays the correct text', () => {
+    const { asFragment } = render(<UnderlinedText {...defaultProps} />);
+    const textElement = screen.getByTestId(UNDERLINED_TEXT_TEST_ID);
+
+    expect(textElement).toHaveTextContent('Underlined Text');
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
