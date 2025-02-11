b523d7ec3fc22a16b93ae9dead69760b8cdc4149
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-11 12:25:21 +0200

    feat: add List component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import { ListProps } from './types';
+import { LIST_TEST_ID, LIST_ITEM_TEST_ID } from '@/lib/testIDs';
+
+const List = ({ list }: ListProps) => {
+  return (
+    <ul
+      className="flex flex-col gap-3 tablet:gap-4 desktop:gap-4"
+      data-testid={LIST_TEST_ID}
+    >
+      {list.map(item => (
+        <li
+          key={item}
+          data-testid={LIST_ITEM_TEST_ID}
+          className="align-center flex gap-2 text-base tablet:gap-3 tablet:text-lg desktop:gap-4 desktop:text-lg"
+        >
+          <div className="relative h-4 w-4 tablet:h-5 tablet:w-5 desktop:h-6 desktop:w-6">
+            <Image src="/icons/cards/check.svg" fill alt="" />
+          </div>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
