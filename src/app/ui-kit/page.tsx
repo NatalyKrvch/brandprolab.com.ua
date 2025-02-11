44a65c3a5242e3a23e60e0ccb731a63b4c34702d
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-11 13:19:36 +0200

    chore: add List component to ui-kit page

--- src/app/ui-kit/page.tsx
+++ src/app/ui-kit/page.tsx
@@ -8,12 +8,20 @@ import {
   InternalLink,
   Label,
   UnderlinedText,
+  List,
 } from '@/components';
 
 const Uikit = () => {
   return (
     <>
-      <div className="m-4 h-[300] w-[442]">
+      <div className="m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">List component</span>
+        <List list={['Item 1', 'Item 2', 'Item 3']} />
+      </div>
+      <div className="m-4 flex h-[300] w-[442] flex-col gap-5">
+        <span className="my-3 text-xl font-bold">
+          TestimonialCard component
+        </span>
         <TestimonialCard
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           clientName="Anastasiia Kovalchuk"
@@ -21,45 +29,55 @@ const Uikit = () => {
           clientLink="https://www.google.com/"
         />
       </div>
-      <div className="m-4 w-[442]">
+      <div className="m-4 flex w-[442] flex-col gap-5">
+        <span className="my-3 text-xl font-bold">
+          TestimonialCard with no client component
+        </span>
         <TestimonialCard text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
       </div>
-      <div className="w-50">
+      <div className="w-50 m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">UserBadge component</span>
         <UserBadge
           userName="John Doe"
           userPhotoUrl="https://media.vanityfair.com/photos/659d6933b6da4325190e2751/1:1/w_1333,h_1333,c_limit/Lindsay-Lohan.jpg"
         />
       </div>
-      <div className="m-4">
+      <div className="m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">UnderlinedText component</span>
         <UnderlinedText>Underlined Text</UnderlinedText>
       </div>
 
-      <div className="m-4 w-[464px]">
+      <div className="m-4 flex w-[464px] flex-col gap-5">
+        <span className="my-3 text-xl font-bold">Button component</span>
         <Button>Записатись</Button>
       </div>
-      <div className="m-4">
+      <div className="m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">Label component</span>
         <Label>Ваш розвиток починається тут!</Label>
       </div>
-      <div className="m-4">
-        <Icon size="l" iconURL="/icons/nav/hand.svg" iconAlt="icon" />
+      <div className="m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">Icon component</span>
+        <Icon size="l" iconURL="/icons/cards/hand.svg" iconAlt="icon" />
       </div>
-      <div className="m-4">
-        <Icon size="m" iconURL="/icons/nav/hand.svg" iconAlt="icon" />
+      <div className="m-4 flex flex-col gap-5">
+        <Icon size="m" iconURL="/icons/cards/hand.svg" iconAlt="icon" />
       </div>
       <div className="m-4">
         <Icon
           size="s"
-          iconURL="/icons/nav/hand.svg"
+          iconURL="/icons/cards/hand.svg"
           iconAlt="icon"
           isIconCentered
         />
       </div>
-      <div className="m-4">
+      <div className="m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">InternalLink component</span>
         <InternalLink href="/" className="font-bold underline">
           Back
         </InternalLink>
       </div>
-      <div className="m-4">
+      <div className="m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">ExternalLink component</span>
         <ExternalLink
           href="https://tailwindcss.com/"
           className="font-bold underline"
@@ -86,49 +104,53 @@ const Uikit = () => {
           </svg>
         </ExternalLink>
       </div>
-      <Container>
-        <p className="border-x-2 border-b-2 font-bold">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor
-          massa, commodo sit amet urna ac, tincidunt luctus ligula. Nam et
-          laoreet ante. Nunc id ultrices enim. Vestibulum scelerisque nisl nec
-          gravida malesuada. Nunc massa elit, rhoncus vitae malesuada et,
-          vulputate et ligula. Integer volutpat ut lectus pretium accumsan.
-          Donec mi urna, semper ac cursus eget, hendrerit quis mi. Sed ut leo
-          non odio tincidunt facilisis. Maecenas a orci augue. Phasellus porta
-          rutrum felis. Sed gravida leo id nulla mattis, iaculis iaculis massa
-          maximus. Maecenas pretium nibh et maximus condimentum. Mauris
-          efficitur elit malesuada lectus pulvinar finibus. Cras ex est, dapibus
-          a risus laoreet, efficitur varius turpis. Mauris elit orci, ornare non
-          semper nec, pharetra eu est. Mauris eget risus ante. Cras quis feugiat
-          leo. Sed fringilla ullamcorper congue. Curabitur vitae accumsan ante.
-          Nam nec tempor dui. Curabitur massa odio, tempus eget sem sit amet,
-          volutpat cursus neque. Ut consequat maximus sodales. Curabitur dui
-          libero, semper ut nulla pharetra, viverra feugiat elit. Ut fermentum
-          mauris turpis, dignissim lacinia purus consectetur ac. Proin metus
-          metus, sagittis eget vehicula at, sagittis id quam. Fusce vestibulum
-          mauris sem, sit amet ultrices arcu sodales eu. Aliquam sed tortor eget
-          urna vulputate pellentesque. Praesent vel justo facilisis, ultricies
-          est eget, sagittis lectus. Sed eget convallis lacus. Vestibulum ante
-          ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-          curae; Suspendisse quis blandit massa, ac mollis lectus. Sed hendrerit
-          nibh in erat consequat fermentum. Etiam egestas sit amet odio at
-          ultrices. Duis nec metus sollicitudin, pretium ante quis, interdum ex.
-          Proin sed interdum ipsum. Vivamus interdum euismod viverra. Nulla
-          interdum ligula quis quam interdum, eget blandit lectus cursus. Fusce
-          euismod ac lorem eget lobortis. Fusce quis blandit ligula. Nulla
-          pulvinar purus sit amet pellentesque elementum. Nunc non pellentesque
-          purus. Fusce fringilla ex mauris, non ullamcorper ex mattis vel.
-          Suspendisse sed erat ut dui condimentum vestibulum quis nec lacus.
-          Etiam porttitor, tellus sit amet fermentum blandit, nisi est pretium
-          nulla, ut tempor neque metus vitae neque. Nullam aliquet convallis
-          metus id mollis. Etiam finibus turpis turpis, sit amet pulvinar quam
-          pretium vel. Suspendisse tempor dapibus lorem vel porta. Sed accumsan
-          felis aliquet ornare blandit. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas. Fusce in
-          suscipit nunc. Donec ac aliquet augue, et imperdiet neque. Quisque
-          quis commodo arcu. Nullam eu magna neque.
-        </p>
-      </Container>
+      <div className="m-4 flex flex-col gap-5">
+        <span className="my-3 text-xl font-bold">Container component</span>
+        <Container>
+          <p className="border-x-2 border-b-2 font-bold">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dolor
+            massa, commodo sit amet urna ac, tincidunt luctus ligula. Nam et
+            laoreet ante. Nunc id ultrices enim. Vestibulum scelerisque nisl nec
+            gravida malesuada. Nunc massa elit, rhoncus vitae malesuada et,
+            vulputate et ligula. Integer volutpat ut lectus pretium accumsan.
+            Donec mi urna, semper ac cursus eget, hendrerit quis mi. Sed ut leo
+            non odio tincidunt facilisis. Maecenas a orci augue. Phasellus porta
+            rutrum felis. Sed gravida leo id nulla mattis, iaculis iaculis massa
+            maximus. Maecenas pretium nibh et maximus condimentum. Mauris
+            efficitur elit malesuada lectus pulvinar finibus. Cras ex est,
+            dapibus a risus laoreet, efficitur varius turpis. Mauris elit orci,
+            ornare non semper nec, pharetra eu est. Mauris eget risus ante. Cras
+            quis feugiat leo. Sed fringilla ullamcorper congue. Curabitur vitae
+            accumsan ante. Nam nec tempor dui. Curabitur massa odio, tempus eget
+            sem sit amet, volutpat cursus neque. Ut consequat maximus sodales.
+            Curabitur dui libero, semper ut nulla pharetra, viverra feugiat
+            elit. Ut fermentum mauris turpis, dignissim lacinia purus
+            consectetur ac. Proin metus metus, sagittis eget vehicula at,
+            sagittis id quam. Fusce vestibulum mauris sem, sit amet ultrices
+            arcu sodales eu. Aliquam sed tortor eget urna vulputate
+            pellentesque. Praesent vel justo facilisis, ultricies est eget,
+            sagittis lectus. Sed eget convallis lacus. Vestibulum ante ipsum
+            primis in faucibus orci luctus et ultrices posuere cubilia curae;
+            Suspendisse quis blandit massa, ac mollis lectus. Sed hendrerit nibh
+            in erat consequat fermentum. Etiam egestas sit amet odio at
+            ultrices. Duis nec metus sollicitudin, pretium ante quis, interdum
+            ex. Proin sed interdum ipsum. Vivamus interdum euismod viverra.
+            Nulla interdum ligula quis quam interdum, eget blandit lectus
+            cursus. Fusce euismod ac lorem eget lobortis. Fusce quis blandit
+            ligula. Nulla pulvinar purus sit amet pellentesque elementum. Nunc
+            non pellentesque purus. Fusce fringilla ex mauris, non ullamcorper
+            ex mattis vel. Suspendisse sed erat ut dui condimentum vestibulum
+            quis nec lacus. Etiam porttitor, tellus sit amet fermentum blandit,
+            nisi est pretium nulla, ut tempor neque metus vitae neque. Nullam
+            aliquet convallis metus id mollis. Etiam finibus turpis turpis, sit
+            amet pulvinar quam pretium vel. Suspendisse tempor dapibus lorem vel
+            porta. Sed accumsan felis aliquet ornare blandit. Pellentesque
+            habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Fusce in suscipit nunc. Donec ac aliquet augue, et
+            imperdiet neque. Quisque quis commodo arcu. Nullam eu magna neque.
+          </p>
+        </Container>
+      </div>
     </>
   );
 };
