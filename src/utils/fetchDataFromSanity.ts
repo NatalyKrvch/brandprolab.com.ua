21a8575f7fd8dce583eac3a9251aef6f87dcd821
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-12 14:13:23 +0200

    refactor: improve throwError function

--- src/utils/fetchDataFromSanity.ts
+++ src/utils/fetchDataFromSanity.ts
@@ -5,7 +5,6 @@ export async function fetchDataFromSanity<T>(query: string): Promise<T> {
   try {
     return await client.fetch<T>(query);
   } catch (error) {
-    console.error(`Error fetching data:`, error);
     throwError(`Failed to fetch data.`);
   }
 }
--- src/utils/throwError.ts
+++ src/utils/throwError.ts
@@ -1,3 +1,4 @@
 export function throwError(message: string): never {
+  console.error(`Error: ${message}`);
   throw new Error(message);
 }
