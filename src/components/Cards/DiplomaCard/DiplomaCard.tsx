9141590683a4c236a572fd3574b4e7879368aeab
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-14 16:17:33 +0200

    feat: add DiplomaCard component

---
+++ src/components/Cards/DiplomaCard/DiplomaCard.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+
+import {
+  DIPLOMA_CARD_TEST_ID,
+  DIPLOMA_IMAGE_TEST_ID,
+  DIPLOMA_OVERLAY_TEST_ID,
+} from '@/lib/testIDs';
+
+import { DiplomaCardProps } from './types';
+
+const DiplomaCard = ({ diplomaURL, onClick }: DiplomaCardProps) => {
+  return (
+    <div
+      className="w-fit rounded-24 border border-gray-medium p-2"
+      data-testid={DIPLOMA_CARD_TEST_ID}
+    >
+      <div className="relative h-40 w-auto max-w-full overflow-hidden rounded-3xl">
+        <div
+          onClick={onClick}
+          className="absolute inset-0 z-10 bg-teal-fog opacity-20 transition-opacity duration-300 hover:cursor-pointer hover:opacity-0"
+          data-testid={DIPLOMA_OVERLAY_TEST_ID}
+        ></div>
+        <Image
+          src={diplomaURL}
+          alt="Diploma"
+          width={168}
+          height={168}
+          className="h-full w-auto object-contain"
+          loading="lazy"
+          data-testid={DIPLOMA_IMAGE_TEST_ID}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiplomaCard;
---
+++ src/components/Cards/DiplomaCard/index.ts
@@ -0,0 +1,1 @@
+export { default as DiplomaCard } from './DiplomaCard';
