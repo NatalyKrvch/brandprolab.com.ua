d4f3c26babb9b63ca815e6f8235bafdb22ebe84f
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-02-16 22:04:46 +0200

    chore: update eslint & prittier rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,21 @@
-import eslintPluginNext from '@next/eslint-plugin-next';
+import { FlatCompat } from '@eslint/eslintrc';
 import eslintPluginTypescript from '@typescript-eslint/eslint-plugin';
 import eslintParserTypescript from '@typescript-eslint/parser';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginEslintComments from 'eslint-plugin-eslint-comments';
 import eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintPluginReact from 'eslint-plugin-react';
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 import eslintPluginTailwindcss from 'eslint-plugin-tailwindcss';
 import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
 
-export default [
+const compat = new FlatCompat({
+  baseDirectory: import.meta.url,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals'),
+
   {
     files: ['src/**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
@@ -21,33 +25,20 @@ export default [
     },
     plugins: {
       '@typescript-eslint': eslintPluginTypescript,
-      prettier: eslintPluginPrettier,
       tailwindcss: eslintPluginTailwindcss,
       import: eslintPluginImport,
       'jsx-a11y': eslintPluginJsxA11y,
       react: eslintPluginReact,
       'react-hooks': eslintPluginReactHooks,
-      '@next/next': eslintPluginNext,
       'eslint-comments': eslintPluginEslintComments,
       'simple-import-sort': eslintPluginSimpleImportSort,
     },
     settings: {
-      react: {
-        version: 'detect',
-      },
-      'import/resolver': {
-        typescript: {},
-        node: {
-          paths: ['src'],
-        },
-      },
-      tailwindcss: {
-        callees: ['cn', 'clsx', 'classnames'],
-      },
+      react: { version: 'detect' },
+      'import/resolver': { typescript: {}, node: { paths: ['src'] } },
+      tailwindcss: { callees: ['cn', 'clsx'] },
     },
     rules: {
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
-      '@next/next/no-html-link-for-pages': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_' },
@@ -66,5 +57,12 @@ export default [
       'eslint-comments/no-unused-disable': 'warn',
     },
   },
-  eslintConfigPrettier,
+  {
+    files: ['**/*.test.{js,jsx,ts,tsx}', '**/*.spec.{js,jsx,ts,tsx}'],
+    rules: {
+      '@next/next/no-img-element': 'off',
+    },
+  },
 ];
+
+export default eslintConfig;
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
-export default {
+const prettierConfig = {
   arrowParens: 'avoid',
   bracketSameLine: false,
   bracketSpacing: true,
@@ -14,3 +14,5 @@ export default {
   trailingComma: 'all',
   endOfLine: 'lf',
 };
+
+export default prettierConfig;
