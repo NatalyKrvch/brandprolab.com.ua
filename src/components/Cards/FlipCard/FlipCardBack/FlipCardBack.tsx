7f933ffeb060b7959439161de3e3b0cab9c41ab1
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-03-11 15:04:08 +0200

    refactor: FlipCardBack component

--- src/components/Cards/FlipCard/FlipCardBack/FlipCardBack.tsx
+++ src/components/Cards/FlipCard/FlipCardBack/FlipCardBack.tsx
@@ -1,6 +1,3 @@
-import clsx from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
 import {
   FLIP_CARD_BACK_HEADER_TEST_ID,
   FLIP_CARD_BACK_LIST_ITEM_TEST_ID,
@@ -8,41 +5,27 @@ import {
 } from '@/lib/testIDs';
 
 import { SimpleCard } from '../../SimpleCard';
-import { FlipCardBackProps } from './types';
+import {
+  getCardWrapperClass,
+  getHeaderClass,
+  getListClass,
+  getListItemClass,
+} from './helpers/getFlipCardBackClasses';
+import type { FlipCardBackProps } from './types';
 
 const FlipCardBack = ({ header, list }: FlipCardBackProps) => {
   return (
-    <SimpleCard
-      className={twMerge(
-        clsx('flex h-full flex-col gap-6 p-10', 'bg-blue-dark'),
-      )}
-    >
+    <SimpleCard className={getCardWrapperClass()}>
       <p
-        className={twMerge(
-          clsx(
-            'text-lg font-bold leading-5 text-teal-medium',
-            'tablet:text-2xl tablet:leading-6',
-            'desktop:text-28 desktop:leading-7',
-          ),
-        )}
+        className={getHeaderClass()}
         data-testid={FLIP_CARD_BACK_HEADER_TEST_ID}
       >
         {header}
       </p>
-      <ul
-        className={twMerge(
-          clsx(
-            'list-disc pl-5',
-            'text-sm font-normal leading-18 text-white',
-            'tablet:text-base tablet:leading-6',
-            'desktop:text-lg desktop:leading-5',
-          ),
-        )}
-        data-testid={FLIP_CARD_BACK_LIST_TEST_ID}
-      >
+      <ul className={getListClass()} data-testid={FLIP_CARD_BACK_LIST_TEST_ID}>
         {list.map(item => (
           <li
-            className="my-2"
+            className={getListItemClass()}
             key={item}
             data-testid={FLIP_CARD_BACK_LIST_ITEM_TEST_ID}
           >
