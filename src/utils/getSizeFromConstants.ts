3a7336f8b9f6415d12084f98ad5ee45c36bafcd4
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-03-11 14:35:09 +0200

    chore: remove unused util

--- src/utils/getSizeFromConstants.ts
+++
@@ -1,9 +0,0 @@
-import { Screen, Size } from '@/lib/types';
-
-export function getSizeFromConstants<T>(
-  constants: Record<Size, Record<Screen, T>>,
-  size: Size,
-  screen: Screen,
-): T {
-  return constants[size][screen];
-}
