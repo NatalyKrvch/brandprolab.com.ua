c9e3158a15a6dc0cee83abe164c475eed2bc2c3c
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-03-11 15:30:06 +0200

    refactor: List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,32 +1,24 @@
-import clsx from 'clsx';
 import Image from 'next/image';
-import { twMerge } from 'tailwind-merge';
 
 import { LIST_ITEM_TEST_ID, LIST_TEST_ID } from '@/lib/testIDs';
 
-import { ListProps } from './types';
+import {
+  getCheckIconWrapperClass,
+  getListItemClass,
+  getListWrapperClass,
+} from './helpers/getListClasses';
+import type { ListProps } from './types';
 
 const List = ({ list }: ListProps) => {
   return (
-    <ul
-      className={twMerge(
-        clsx('flex flex-col gap-3 tablet:gap-4 desktop:gap-4'),
-      )}
-      data-testid={LIST_TEST_ID}
-    >
+    <ul className={getListWrapperClass()} data-testid={LIST_TEST_ID}>
       {list.map(item => (
         <li
           key={item}
+          className={getListItemClass()}
           data-testid={LIST_ITEM_TEST_ID}
-          className={twMerge(
-            clsx(
-              'flex items-center gap-2 text-base',
-              'tablet:gap-3 tablet:text-lg',
-              'desktop:gap-4 desktop:text-lg',
-            ),
-          )}
         >
-          <div className="relative h-4 w-4 tablet:h-5 tablet:w-5 desktop:h-6 desktop:w-6">
+          <div className={getCheckIconWrapperClass()}>
             <Image
               src="/icons/cards/check.svg"
               fill
---
+++ src/components/List/helpers/getListClasses.ts
@@ -0,0 +1,8 @@
+export const getListWrapperClass = () =>
+  'flex flex-col gap-3 tablet:gap-4 desktop:gap-4';
+
+export const getListItemClass = () =>
+  'flex items-center gap-2 text-base tablet:gap-3 tablet:text-lg desktop:gap-4 desktop:text-lg';
+
+export const getCheckIconWrapperClass = () =>
+  'relative h-4 w-4 tablet:h-5 tablet:w-5 desktop:h-6 desktop:w-6';
