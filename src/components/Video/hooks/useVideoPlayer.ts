b550b6fe5bd6ae8050c35264c40f6fd986346f02
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-03-11 15:41:32 +0200

    refactor: Video component

---
+++ src/components/Video/hooks/useVideoPlayer.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export const useVideoPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
+  return {
+    isPlaying,
+    handlePlay,
+  };
+};
