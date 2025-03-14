487a86a1b2c7099cc5f5b720254ec870bbdb9522
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-03-14 20:45:09 +0200

    fix: offset logic in Icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -6,7 +6,6 @@ import {
 } from '@/lib/testIDs';
 
 import { getIconClasses } from './helpers/getIconClasses';
-import { useIconOffset } from './hooks/useIconOffset';
 import { IconProps } from './types';
 
 const Icon = ({
@@ -16,7 +15,6 @@ const Icon = ({
   type,
 }: IconProps) => {
   const { circleClass, iconClass } = getIconClasses(type);
-  const offset = useIconOffset(type);
 
   return (
     <div
@@ -30,9 +28,6 @@ const Icon = ({
           width={0}
           height={0}
           className={iconClass}
-          style={{
-            transform: `translateX(-${offset}px)`,
-          }}
           data-testid={ICON_COMPONENT_IMAGE_TEST_ID}
         />
       </div>
--- src/components/Icon/helpers/getIconClasses.ts
+++ src/components/Icon/helpers/getIconClasses.ts
@@ -11,45 +11,25 @@ export const getIconClasses = (type: IconClassType): IconClassSet => {
     case IconClassType.SERVICES:
       return {
         circleClass: `${baseCircleClass} w-[68px] h-[68px] tablet:w-[80px] tablet:h-[80px] desktop:w-[108px] desktop:h-[108px]`,
-        iconClass: `${baseIconClass} w-[64px] h-[64px] tablet:w-[72px] tablet:h-[72px] desktop:w-[96px] desktop:h-[96px]`,
-        offsets: {
-          mobile: 24,
-          tablet: 30,
-          desktop: 36,
-        },
+        iconClass: `${baseIconClass} w-[64px] h-[64px] tablet:w-[72px] tablet:h-[72px] desktop:w-[96px] desktop:h-[96px] translate-x-[-24px] tablet:translate-x-[-30px] desktop:translate-x-[-36px]`,
       };
 
     case IconClassType.EXPERTISE:
       return {
         circleClass: `${baseCircleClass} w-[68px] h-[68px] tablet:w-[80px] tablet:h-[80px] desktop:w-[64px] desktop:h-[64px]`,
-        iconClass: `${baseIconClass} w-[64px] h-[64px] tablet:w-[72px] tablet:h-[72px] desktop:w-[64px] desktop:h-[64px]`,
-        offsets: {
-          mobile: 24,
-          tablet: 30,
-          desktop: 18,
-        },
+        iconClass: `${baseIconClass} w-[64px] h-[64px] tablet:w-[72px] tablet:h-[72px] desktop:w-[64px] desktop:h-[64px] translate-x-[-24px] tablet:translate-x-[-30px] desktop:translate-x-[-18px]`,
       };
 
     case IconClassType.SOCIAL_MEDIA:
       return {
         circleClass: `${baseCircleClass} w-[36px] h-[36px] tablet:w-[42px] tablet:h-[42px] desktop:w-[48px] desktop:h-[48px]`,
         iconClass: `${baseIconClass} w-[22px] h-[22px] tablet:w-[28px] tablet:h-[28px] desktop:w-[32px] desktop:h-[32px]`,
-        offsets: {
-          mobile: 0,
-          tablet: 0,
-          desktop: 0,
-        },
       };
 
     default:
       return {
         circleClass: baseCircleClass,
         iconClass: baseIconClass,
-        offsets: {
-          mobile: 0,
-          tablet: 0,
-          desktop: 0,
-        },
       };
   }
 };
