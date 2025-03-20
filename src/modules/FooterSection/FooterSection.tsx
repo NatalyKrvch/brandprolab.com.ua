94b3c22c40f35fdcf893dc3a1d6b1d72d4389cce
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-03-20 20:01:58 +0200

    feat: add FooterSection

---
+++ src/modules/FooterSection/FooterSection.tsx
@@ -0,0 +1,32 @@
+import { ExternalLink } from '@/components/ExternalLink';
+import { FOOTER_SECTION_TEST_ID } from '@/lib/testIDs';
+import { getCurrentYear } from '@/utils/getCurrentYear';
+
+import { FooterSectionProps } from './types';
+
+const FooterSection = ({ offerURL }: FooterSectionProps) => {
+  const year = getCurrentYear();
+
+  return (
+    <footer
+      data-testid={FOOTER_SECTION_TEST_ID}
+      className="border-t border-gray-medium px-2 py-4 tablet:px-4 tablet:py-8 desktop:px-4 desktop:py-8"
+    >
+      <div className="flex items-center justify-between text-xs text-gray-dark tablet:text-sm desktop:text-sm">
+        <div className="flex flex-col justify-between tablet:w-1/2 tablet:flex-row desktop:w-1/2 desktop:flex-row">
+          <div>ФОП Захарова Вікторія</div>
+          <div>(c) {year}</div>
+        </div>
+
+        <ExternalLink
+          href={offerURL}
+          className="whitespace-nowrap underline transition hover:text-teal-dark"
+        >
+          Договір оферти
+        </ExternalLink>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterSection;
