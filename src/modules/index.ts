64fe4737f0eac9bcb81e240500edbd21a6db387b
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-05-15 15:10:45 +0300

    docs: add TestimonialsSection to modules page

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,4 @@
 export { DiagnosticSection } from './DiagnosticSection';
 export { FooterSection } from './FooterSection';
+export { TestimonialsSection } from './TestimonialsSection';
 export { VideoSection } from './VideoSection';
