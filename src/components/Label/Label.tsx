c26466c72f52367c5364157783ecfd55949c3bb2
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-06-16 13:12:05 +0300

    fix: Label component styles

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -10,7 +10,7 @@ const Label = ({ children, className }: PropsWithChildren<LabelProps>) => {
     <div
       data-testid={LABEL_COMPONENT_TEST_ID}
       className={twMerge(
-        'flex max-w-max items-center justify-center rounded-full bg-teal-lightOpacity text-blue-dark',
+        'flex max-w-max items-center justify-center rounded-40 bg-teal-lightOpacity px-4 py-2 text-blue-dark',
         className,
       )}
     >
