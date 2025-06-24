4751420eeaddbe5ecc652a9c30dfff455f1322b1
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-06-24 23:33:12 +0300

    fix: Button animation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,7 +15,7 @@ const Button = ({
       data-testid={BUTTON_TEST_ID}
       onClick={onClick}
       className={clsx(
-        'w-full rounded-full py-3 text-xl font-medium leading-26 tracking-wide outline-none transition-all duration-300 hover:outline-offset-0 tablet:py-5 tablet:text-22 desktop:py-6 desktop:text-2xl desktop:leading-6',
+        'w-full rounded-full py-3 text-xl font-medium leading-26 tracking-wide outline-none transition-all duration-150 ease-in-out hover:outline-offset-0 tablet:py-5 tablet:text-22 desktop:py-6 desktop:text-2xl desktop:leading-6',
         variant === 'white'
           ? 'bg-white font-normal text-blue-default hover:outline-8 hover:outline-whiteOpacity active:bg-teal-light'
           : 'bg-teal-dark text-white hover:outline-8 hover:outline-teal-darkTranslucent active:bg-black',
