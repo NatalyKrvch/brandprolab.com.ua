3b86864ea78f78e054d8752c67887d3da32d163f
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-07-11 15:45:25 +0300

    feat: add HeroTextContent component

---
+++ src/components/HeroTextContent/types.ts
@@ -0,0 +1,15 @@
+export interface HeroTextContentProps {
+  iconUrl: string;
+  title: string;
+  subtitle: string;
+  description: string;
+  buttonText: string;
+  buttonLink: string;
+  iconClassName?: string;
+  titleClassName?: string;
+  titleSpanClassName?: string;
+  subtitleClassName?: string;
+  descriptionClassName?: string;
+  wrapperClassName?: string;
+  contentWrapperClassName?: string;
+}
