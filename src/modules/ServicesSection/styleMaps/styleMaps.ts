9ed4d86c8c79d724196b997abaefc5e1d05664e8
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-08-04 18:47:36 +0300

    feat: add expertiseClass to ServicesSection styleMaps

--- src/modules/ServicesSection/styleMaps/styleMaps.ts
+++ src/modules/ServicesSection/styleMaps/styleMaps.ts
@@ -11,9 +11,13 @@ const fullWidthClass =
 
 const defaultClass = 'desktop:h-500 desktop:max-h-500';
 
+const expertiseClass =
+  'w-full min_mobile:h-[320px] min_mobile:w-[288px] [@media(min-width:380px)_and_(max-width:500px)]:h-[320px] [@media(min-width:380px)_and_(max-width:500px)]:w-[350px] [@media(min-width:500px)_and_(max-width:600px)]: h-[400px] w-[350px] [@media(min-width:500px)_and_(max-width:600px)]:h-[320px] [@media(min-width:500px)_and_(max-width:600px)]:w-[460px] [@media(min-width:600px)_and_(max-width:900px)]:h-[320px] [@media(min-width:600px)_and_(max-width:900px)]:w-[550px] tablet:h-[320px] tablet:w-[415px] mid_tablet:h-[320px] mid_tablet:w-[490px] desktop:h-[360px] desktop:w-[336px]';
+
 export const serviceCardClassMap: Record<ServiceCardFrontVariant, string> = {
   [ServiceCardFrontVariant.DEFAULT]: clsx(baseCardClass, defaultClass),
   [ServiceCardFrontVariant.GRADIENT]: clsx(baseCardClass, fullWidthClass),
+  [ServiceCardFrontVariant.BASE]: expertiseClass,
 };
 
 export const cardIndexToVariantMap: Partial<
