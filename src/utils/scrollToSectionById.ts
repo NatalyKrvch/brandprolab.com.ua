915646d96ff87e2c133cda275478bdf8050bccb9
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-08-16 20:33:18 +0300

    chore: remove obsolent scrollToSectionById util

--- src/utils/scrollToSectionById.ts
+++
@@ -1,7 +0,0 @@
-export const scrollToSectionById = (id: string) => {
-  const section = document.getElementById(id);
-
-  if (section) {
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
-};
