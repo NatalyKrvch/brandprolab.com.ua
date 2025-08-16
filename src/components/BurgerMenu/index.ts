4a48df6d07419e6d5df4703e921b13524365ef67
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-08-16 20:56:55 +0300

    feat: add BurgerMenu component

---
+++ src/components/BurgerMenu/index.ts
@@ -0,0 +1,2 @@
+export { default as BurgerMenu } from './BurgerMenu';
+export * from './types';
---
+++ src/components/BurgerMenu/types.ts
@@ -0,0 +1,13 @@
+import type { MenuItemType } from '../NavigationMenu';
+import type { SocialIcon } from '../SocialIcons';
+
+export interface BurgerMenuProps {
+  isOpen: boolean;
+  onClose: () => void;
+  socialLinks: SocialIcon[];
+  menuItems: MenuItemType[];
+  panelClassName?: string;
+  navClassName?: string;
+  animationMs?: number;
+  portalId?: string;
+}
